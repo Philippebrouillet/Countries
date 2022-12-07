3604ab46f89a679736c54dcb5d6914455fdbcd66
Brouillet <philippebrouillet@icloud.com>
2022-12-07 15:19:09 +0100

    //

---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Logo() {
+  return (
+    <div className="logo">
+      <img src="./logo192.png" alt="logo react" />
+      <h3>React Wolrd</h3>
+    </div>
+  );
+}
+
+export default Logo;
---
+++ src/pages/About.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+function About() {
+  return (
+    <div>
+      <Logo />
+      <Navigation /> <br />
+      <h1>A propos</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi nesciunt
+        itaque aperiam pariatur nam eligendi molestias porro amet ipsa
+        perferendis? Ipsum omnis debitis minus, alias accusamus, autem quibusdam
+        expedita cum illo numquam quae eum possimus. Nemo veritatis et qui quas,
+        fuga perspiciatis provident nobis dolorem delectus molestias nostrum
+        aspernatur facere sapiente ex eos minima. Nisi, illum sapiente nihil
+        beatae commodi a dolorem perferendis ipsa ratione harum voluptatem saepe
+        iure maxime sit tenetur, in quis labore! Sapiente numquam illo quasi
+        deleniti itaque, ipsam incidunt nemo? Eum veniam natus quos ipsam
+        nesciunt officiis porro? Nulla neque doloribus veniam veritatis est
+        dicta molestias voluptate, cumque corrupti iure et quam aliquam ad,
+        necessitatibus officia rem. Accusamus nisi a molestias corrupti ut modi
+        natus ab aperiam obcaecati iure nulla magnam, aut, expedita odio
+        incidunt fugiat magni! Consectetur, dolore similique nesciunt, veniam
+        eligendi corrupti cum beatae eos nisi, molestiae aliquid magni maxime
+        consequuntur nam quis minima. Et rem quasi totam vitae itaque tempore
+        similique impedit illo quibusdam molestias dolore consequuntur neque
+        beatae pariatur laborum ab deleniti ullam, nulla labore voluptas saepe
+        ipsum. Natus quidem consequatur mollitia aperiam quibusdam ea cum,
+        molestias nisi aliquid facere sapiente expedita veritatis illum illo
+        omnis quia nobis neque aliquam aut ratione?
+      </p>
+    </div>
+  );
+}
+
+export default About;
